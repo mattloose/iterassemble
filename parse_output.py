fd716919433efe4672fd7cec4af681727659261c
Teri Evans <teri.evans@nottingham.ac.uk>
2017-02-02 16:53:50 +0000

    Replaced BLAST with re.finditer in parse_output.py

--- parse_output.py
+++ parse_output.py
@@ -4,6 +4,7 @@ import subprocess
 import os
 import argparse
 from Bio import SeqIO
+import re
 
 parser = argparse.ArgumentParser(description='Parse vgw output using gmap')
 parser.add_argument('genome', help='vgw output file')
@@ -38,21 +39,24 @@ with open(args.gff, 'r') as ins:
         gmapres[data[0]].append(int(data[3]))
 ins.close()
 
-subprocess.call("makeblastdb -in "+args.genome+" -dbtype nucl", shell=True)
+# subprocess.call("makeblastdb -in "+args.genome+" -dbtype nucl", shell=True)
+#
+# blastres = dict()
+#
+# p2 = subprocess.Popen("blastn -db "+args.genome+" -query "+args.genome+" -outfmt 6 | sort -n -k 7", shell=True, stdout=subprocess.PIPE)
+#
+# for l in iter(p2.stdout.readline,''):
+#     l = l.rstrip()
+#     data = l.split("\t")
+#     if data[0] != data[1] or data[2] != "100.00":
+#         continue
+#     #print data
+#     if data[0] not in blastres:
+#         blastres[data[0]] = []
+#     blastres[data[0]].append(int(data[6]))
 
-blastres = dict()
-
-p2 = subprocess.Popen("blastn -db "+args.genome+" -query "+args.genome+" -outfmt 6 | sort -n -k 7", shell=True, stdout=subprocess.PIPE)
-
-for l in iter(p2.stdout.readline,''):
-    l = l.rstrip()
-    data = l.split("\t")
-    if data[0] != data[1] or data[2] != "100.00":
-        continue
-    #print data
-    if data[0] not in blastres:
-        blastres[data[0]] = []
-    blastres[data[0]].append(int(data[6]))
+chunkbreak = "N"*500
+#print chunkbreak
 
 outfile = args.genome+"_gmapparsed"
 out = open(outfile, 'w')
@@ -60,33 +64,48 @@ out = open(outfile, 'w')
 seqlens = []
 
 for record in SeqIO.parse(args.genome, "fasta"):
+    #print record.name
     newseq = []
+    startlist = []
+    endlist = []
+
     if record.name not in gmapres:
         continue
+
+    startlist.append(0)
+    for m in re.finditer(chunkbreak, str(record.seq)):
+        #print "Break: %s - %s" % (m.start(), m.end())
+        startlist.append(m.end())
+        endlist.append(m.start())
+
+    endlist.append(len(record.seq))
     #print record.name
     #print gmapres[record.name]
     #print blastres[record.name]
-    for a in range(len(blastres[record.name])-1):
-        cstart = blastres[record.name][a]
-        nextstart = blastres[record.name][a+1]
+    for a in range(len(startlist)):
+        #print "%s: %s - %s" % (a, startlist[a], endlist[a])
+        # cstart = blastres[record.name][a]
+        # nextstart = blastres[record.name][a+1]
         #print str(cstart)+" - "+str(nextstart)
         for g in gmapres[record.name]:
             #print "\t"+str(g)
-            if g >= cstart and g < nextstart:
+            if g >= startlist[a] and g < endlist[a]:
                 #print "This is a good chunk!"
-                newseq.append(str(record.seq[cstart-1:nextstart-501]))
+                newseq.append(str(record.seq)[startlist[a]:endlist[a]])
                 break
 
-    laststart = blastres[record.name][-1]
-    #print laststart
-    for g in gmapres[record.name]:
-        if g >= laststart:
-            #print "Last chunk is good!"
-            newseq.append(str(record.seq[laststart-1:len(record.seq)]))
-            break
+    # laststart = blastres[record.name][-1]
+    # #print laststart
+    # for g in gmapres[record.name]:
+    #     if g >= laststart:
+    #         #print "Last chunk is good!"
+    #         newseq.append(str(record.seq[laststart-1:len(record.seq)]))
+    #         break
+    #print newseq
     seqlens.append(len("".join(newseq)))
     out.write(">"+record.name+"\n")
     out.write(("N"*500).join(newseq)+"\n")
+    #break
 
 seqlens = sorted(seqlens)
 print "Num: "+str(len(seqlens))
